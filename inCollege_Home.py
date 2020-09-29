b3e03c61630d6f70658b3a2c80b5316ed2c48951
Ali Bayatpoor <alizbay94@gmail.com>
2020-09-28 23:00:50 -0400

    Third Sprint Functionality Completed

--- inCollege_Home.py
+++ inCollege_Home.py
@@ -6,6 +6,8 @@ import inCollege_Database as database
 import inCollege_CurrentUser as user
 import time
 
+#Note - When going back, and when an error occurs, the program sleeps for 1 second for added effect
+
 
 def skillScreen():
     print("\n|*| NOTE - Enter 'x' at any time to go back |*|\n")
@@ -39,9 +41,12 @@ def skillScreen():
             print("Under construction.")
             return True
         elif sel == 'x': #Go back to main menu
+            print("... Going Back")
+            time.sleep(1)
             return False
         else:
-            print("Invalid Input")
+            print("...Invalid Input")
+            time.sleep(1)
 
 
 # def selectionScreen(val):
@@ -78,11 +83,13 @@ def mainMenuIntroMessage():
     played = False
     sel = input("Enter Your Selection:\n")
     while (sel != '0' and sel != '1'):
-        sel = input("Invalid Entry. Enter 0 or 1.")
+        print("... Invalid Entry")
+        time.sleep(1)
+        sel = input("Enter Your Selection: ")
+
 
     if (sel == '1'):
         print("\n\n")
-        time.sleep(1)
         print("|================================================|")
         print("|---------------------|\-------------------------|")
         print("|---------------------|-\------------------------|")
@@ -112,7 +119,7 @@ def main ():
 
     while (sel != 'x'):
 
-        #This menu is displayed to non-logged in user
+        #This menu is displayed to non-logged in user. 
         if (loginStatus == False):
             print("\n")
             print("         + ----------- +")
@@ -132,57 +139,287 @@ def main ():
                 print("       | GOOD BYE! |")
                 print("       + --------- +")
                 time.sleep(1)
-                # Ignore everything between '=====' (Printing DB results)
+                # Ignore everything between '=====' (Printing DB results), Do Not Delete*
                 # ========================================================
                 # db = database.Database()
                 # print("Students in database: ", db.data["Students"])
                 # print("Jobs in database: ", db.data["Jobs"])
                 # =========================================================
-                return 0
+                return 0    
+
             elif sel == '1': #Log In. if log in is successful, the user object is returned. Otherwise, false is returned.
                 theUser = accnt.login(db)
                 if theUser is False:
                     loginStatus = False
                 else:
                     loginStatus = True
-            elif sel == '2': #Create an account
+
+            #Create an account
+            elif sel == '2': 
                 accnt.create_account(db)
 
-            elif sel == '3': #Find Someone You Know
+            #Find Someone You Know
+            elif sel == '3': 
                 foundUser = db.search_users() #returns t / f
                 theUser = False
                 if foundUser == True:
                     print("\nThey are a part of the InCollege system!")
-                    time.sleep(1)
                     print("Why don't you join them?\n")
                     time.sleep(1)
 
-                    while sel != '0': #had to change this to zero to fix problem with program exiting if user places x there
+                    while sel != 'x': #had to change this to zero to fix problem with program exiting if user places x there
                         print("+-------------------------+")
                         print("| 1. Login                |")
                         print("| 2. Create New Account   |")
-                        print("| 0. Go Back to Main Menu |")
+                        print("| x. Go Back to Main Menu |")
                         print("+-------------------------+")
                         sel = input("\nEnter Your Selection: ")
 
                         if sel == '1':
                             theUser = accnt.login(db)
                             if theUser is False:
+                                print("...\n")
+                                time.sleep(1)
                                 loginStatus = False
                             else:
                                 loginStatus = True
                         elif sel == '2':
-                            accnt.create_account(db)
-                            print("Log in from the Main Menu")
-                            break
+                            isAccount = accnt.create_account(db)
+
+                            #If the account was made without the use selecting to go back
+                            if isAccount is True:
+                                print("Log in from the Main Menu")
+                                break
+
+                            elif isAccount is False:
+                                print("...\n")
+                                time.sleep(1)
+
+                        elif sel == 'x':
+                            print("...\n")
+                            time.sleep(1)
                         else:
+                            print("... Invalid Input")
                             time.sleep(1)
-                            print("Invalid selection.")
-                            time.sleep(1)
+
+                    sel = '' #resetting from 'x' to fix problem where programming was exitting 
                 if theUser is False:
                     loginStatus = False
                 else:
                     loginStatus = True
+
+            # Useful Links
+            elif sel == '4':
+                #This will control the loop for Useul Links, when we want to exit the loop the flag will be flipped
+                flag = True
+                while flag is True:
+
+                    print("       + -------------- +")
+                    print("       |  USEFUL LINKS  |         ")
+                    print(" +------------------------------+")
+                    print(" | 1. General                   |")
+                    print(" | 2. Browse inCollege          |")
+                    print(" | 3. Business Solutions        |")
+                    print(" | 4. Directories               |")
+                    print(" | x. Go Back                   |")
+                    print(" +------------------------------+")
+
+                    sel = input("\nEnter Your Selection: ")
+                    
+                    #General
+                    if sel == '1':
+                        flag2 = True
+                        while flag2 is True:
+                            print("    + --------- +")
+                            print("    |  GENERAL  |")
+                            print(" +------------------+")
+                            print(" | 1. Sign Up       |")
+                            print(" | 2. Help Center   |")
+                            print(" | 3. About         |")
+                            print(" | 4. Press         |")
+                            print(" | 5. Blog          |")
+                            print(" | 6. Careers       |")
+                            print(" | 7. Developers    |")
+                            print(" | x. Go Back       |")
+                            print(" +------------------+")
+
+                            sel = input("\nEnter Your Selection: ")
+
+                            if sel == "1":
+                                print("Need to take them to sign up")
+
+                            elif sel == "2":
+                                print()
+                                print("+========================+")
+                                print("|*| We're Here to Help |*|")
+                                print("+========================+\n")
+                            elif sel == "3":
+                                print()
+                                print("+========================+")
+                                print("|*| Our About Goes Here |*|")
+                                print("+========================+\n")
+
+                            elif sel == "4": 
+                                print()
+                                print("+========================+")
+                                print("|*| Under Construction |*|")
+                                print("+========================+\n")
+
+                            elif sel == "5":
+                                print()
+                                print("+========================+")
+                                print("|*| Under Construction |*|")
+                                print("+========================+\n")
+
+                            elif sel == "6":
+                                print()
+                                print("+========================+")
+                                print("|*| Under Construction |*|")
+                                print("+========================+\n")
+
+                            elif sel == "7":
+                                print()
+                                print("+========================+")
+                                print("|*| Under Construction |*|")
+                                print("+========================+\n")
+
+                            elif sel == "x":
+                                flag2 = False
+
+                            else:
+                                print("...Invalid Input")
+                                time.sleep(1)
+
+                        sel = "" #resetting sel
+
+
+                    #Browse inCollege
+                    elif sel == '2':
+                        print()
+                        print("+========================+")
+                        print("|*| Under Construction |*|")
+                        print("+========================+\n")
+                    #Business Solutions
+                    elif sel == '3': 
+                        print()
+                        print("+========================+")
+                        print("|*| Under Construction |*|")
+                        print("+========================+\n")                    
+                    #Directories
+                    elif sel == '4':
+                        print()
+                        print("+========================+")
+                        print("|*| Under Construction |*|")
+                        print("+========================+\n")                    
+                    #Go Back
+                    elif sel == 'x':
+                        flag = False
+
+                    else:
+                        print("...Invalid Input")
+                        time.sleep(1)
+                
+                sel = "" #resetting sel
+
+            # inCollege Important Links
+            elif sel == '5':
+                flag = True
+                while flag is True:
+                    print("+---------------------------+")
+                    print("| inCollege Important Links |")
+                    print("+---------------------------+")
+                    print("| 1. Copyright Notice       |")
+                    print("| 2. About                  |")
+                    print("| 3. Accessibility          |")
+                    print("| 4. User Agreement         |")
+                    print("| 5. Privacy Policy         |")
+                    print("| 6. Cookie Policy          |")
+                    print("| 7. Brand Policy           |")
+                    print("| 8. Languages              |")
+                    print("| x. Go Back                |")
+                    print("+---------------------------+\n")
+
+                    sel = input("Enter Your Selection: ")
+
+                    if sel == "1":
+                        print("Our Copyright notice goes here")
+                    elif sel == "2":
+                        print("Our About goes here")
+
+                    elif sel == "3":
+                        print("Our Accessibility goes here")
+
+                    elif sel == "4":
+                        print("Our User Agreement goes here")
+
+                    elif sel == "5":
+                        print(" +-------------------------+")
+                        print(" |     Privacy Policy      |")
+                        print(" +-------------------------+\n")
+                        print("Our Privacy Policy is written here...\n")
+                        flag2 = True
+                        while flag2 is True:
+                            print("+--------------------------+")
+                            print("|   Edit Guest Controls?   |")
+                            print("+--------------------------+")
+                            print("| 1. Yes                   |")
+                            print("| x. Go Back               |")
+                            print("+--------------------------+\n")
+                            sel = input("Enter Your Selection: ")
+
+                            if sel == "1":
+                                print("Editing Guest Controls")
+
+                            elif sel == "x":
+                                flag2 = False
+                                print("... Going Back")
+                                time.sleep(1)
+
+                            else:
+                                print("... Invalid Input")
+                                time.sleep(1)
+
+                        sel = "" #resetting
+                
+                    elif sel == "5":
+                        print("Our Privacy Policy goes here")
+
+                    elif sel == "6":
+                        print("Our Cookie Policy goes here")
+
+
+                    elif sel == "7":
+                        print("Our Brand Policy goes here")
+
+                    elif sel == "8":
+                        flag2 = True
+                        while flag2 is True:
+                            print("+---------------------------------------+")
+                            print("|   Language Settings Guest Controls?   |")
+                            print("+---------------------------------------+")
+                            print("| 1. Change Settings                    |")
+                            print("| x. Go Back                            |")
+                            print("+---------------------------------------+\n")
+                            sel = input("Enter Your Selection: ")
+
+                            if sel == "1":
+                                print("Change the Settings")
+                            elif sel == "x":
+                                flag2 = False
+                                print("... Going Back\n")
+                                time.sleep(1)
+
+                        sel = "" #resetting sel
+
+                    elif sel == "x":
+                        flag = False
+                        print("... Going Back\n")
+                        time.sleep(1)
+
+                    else:
+                        print("...Invalid Input")
+                        time.sleep(1)
+                sel = "" #resetting
 
             #Erases the database
             elif sel == "-100":
@@ -190,7 +427,7 @@ def main ():
             else:
                 print("...")
                 time.sleep(1)
-                print("Invalid Selection!")
+                print("Invalid Input")
                 time.sleep(1)
 
         # if User is logged in
@@ -199,16 +436,34 @@ def main ():
             print("         + ----------- +")
             print("         |  MAIN MENU  |         ")
             print(" +------------------------------+")
-            print(" | 1. Login                     |")
-            print(" | 2. Create New Account        |")
-            print(" | 3. Find Someone You Know     |")
-            print(" | 4. Useful Links              |")
-            print(" | 5. InCollege Important Links |")
+            print(" | 1. Post a New Job            |")
+            print(" | 2. Find Someone You Know     |")
+            print(" | 3. Skill Screen              |")
+            print(" | 5. Useful Links              |")
+            print(" | 6. InCollege Important Links |")
             print(" | x. Quit                      |")
             print(" +------------------------------+")
             print("")
             sel = input("Enter Your Selection: ")
-            if sel == 'x':
+            
+            # Post A Job
+            if sel == '1': 
+                accnt.post_job(theUser.name, db)
+
+            # Find someone you know
+            elif sel == '2': 
+                db = database.Database()
+                foundUser = db.search_users()
+                if foundUser is True:
+                    print("... User found in the inCollege System!")
+                    time.sleep(1)
+                    
+            # Learn a New Skill        
+            elif sel == '3': 
+                skillScreen()
+
+            #Exit Program    
+            elif sel == 'x':
                 print("       + --------- +")
                 print("       | GOOD BYE! |")
                 print("       + --------- +")
@@ -221,24 +476,11 @@ def main ():
                 # print("Jobs in database: ", db.data["Jobs"])
                 # =========================================================
                 return 0
-            elif sel == '1': # Post A Job
-                accnt.post_job(theUser.name, db)
-            elif sel == '2':
-                print("Under construction.")                
-            elif sel == '3': # Find someone you know
-                db = database.Database()
-                foundUser = db.search_users()
-                if foundUser is True:
-                    print("...")
-                    time.sleep(1)
-                    print("User found in the inCollege System!")
-                    time.sleep(1)
-            elif sel == '4': # Learn a New Skill
-                skillScreen()
             elif sel == "-100":
                 db.clear()
             else:
-                print("Invalid Selection!")
+                print("...Invalid Input")
+                time.sleep(1)
 
 
 
